10d7048d74e7a48c11839c768a9ae263d4991388
Pedro Castro <falecompedroac@gmail.com>
2025-04-01 23:09:33 +0100

    updated db creation script

--- database/create.sql
+++ database/create.sql
@@ -9,23 +9,25 @@ DROP TABLE IF EXISTS Feedback;
 CREATE TABLE User
 (
     userId            INTEGER,
-    name              NVARCHAR NOT NULL,
-    username          VARCHAR  NOT NULL,
-    email             VARCHAR  NOT NULL,
-    password          VARCHAR  NOT NULL,
-    isAdmin           BOOLEAN  NOT NULL,
-    profilePictureURL VARCHAR,
+    name              NVARCHAR(255) NOT NULL,
+    username          VARCHAR(255) NOT NULL,
+    email             VARCHAR(255) NOT NULL,
+    password          VARCHAR(255) NOT NULL,
+    isAdmin           BOOLEAN      NOT NULL DEFAULT 0,
+    profilePictureURL VARCHAR(1023),
+    status            VARCHAR(15),
 
     CONSTRAINT User_PK PRIMARY KEY (userId),
     CONSTRAINT User_email_unique UNIQUE (email),
     CONSTRAINT User_username_unique UNIQUE (username),
-    CONSTRAINT User_isAdmin_CK CHECK ( isAdmin IN (0, 1) )
+    CONSTRAINT User_isAdmin_CK CHECK ( isAdmin IN (0, 1) ),
+    CONSTRAINT User_status_CK CHECK ( status IN ('active', 'inactive') )
 );
 
 CREATE TABLE ServiceCategory
 (
     serviceCategoryId INTEGER,
-    name              NVARCHAR NOT NULL,
+    name              VARCHAR(255) NOT NULL,
 
     CONSTRAINT ServiceCategory_PK PRIMARY KEY (serviceCategoryId)
 );
@@ -35,16 +37,16 @@ CREATE TABLE Service
     serviceId    INTEGER,
     freelancerId INTEGER        NOT NULL,
     categoryId   INTEGER,
-    title        VARCHAR,
+    title        VARCHAR(255)   NOT NULL,
     price        DECIMAL(10, 2) NOT NULL,
     deliveryTime INTEGER        NOT NULL,
     description  TEXT           NOT NULL,
-    status       VARCHAR        NOT NULL,
+    status       VARCHAR(15)    NOT NULL,
     rating       DECIMAL(2, 1)  NOT NULL DEFAULT 0,
 
     CONSTRAINT Service_PK PRIMARY KEY (serviceId),
-    CONSTRAINT Service_freelancer_FK FOREIGN KEY (freelancerId) REFERENCES User (userId) ON DELETE CASCADE,
-    CONSTRAINT Service_category_FK FOREIGN KEY (categoryId) REFERENCES ServiceCategory (serviceCategoryId) ON DELETE SET NULL,
+    CONSTRAINT Service_freelancer_FK FOREIGN KEY (freelancerId) REFERENCES User (userId) ON UPDATE CASCADE,
+    CONSTRAINT Service_category_FK FOREIGN KEY (categoryId) REFERENCES ServiceCategory (serviceCategoryId) ON DELETE SET NULL ON UPDATE CASCADE,
     CONSTRAINT Service_deliveryTime_check CHECK ( deliveryTime > 0 ),
     CONSTRAINT Service_status_CK CHECK ( status IN ('active', 'inactive', 'deleted') ),
     CONSTRAINT Service_rating_CK CHECK ( rating BETWEEN 0 AND 5 )
@@ -53,40 +55,40 @@ CREATE TABLE Service
 CREATE TABLE ServiceMedia
 (
     serviceMediaId INTEGER,
-    serviceId      INTEGER,
-    mediaURL       VARCHAR NOT NULL,
+    serviceId      INTEGER       NOT NULL,
+    mediaURL       VARCHAR(1023) NOT NULL,
 
     CONSTRAINT ServiceMedia_PK PRIMARY KEY (serviceMediaId),
-    CONSTRAINT ServiceMedia_service_FK FOREIGN KEY (serviceId) REFERENCES Service (serviceId) ON DELETE CASCADE
+    CONSTRAINT ServiceMedia_service_FK FOREIGN KEY (serviceId) REFERENCES Service (serviceId) ON UPDATE CASCADE
 );
 
 CREATE TABLE Purchase
 (
     purchaseId INTEGER,
-    serviceId  INTEGER,
-    clientId   INTEGER,
-    date       DATE    NOT NULL,
-    status     VARCHAR NOT NULL,
+    serviceId  INTEGER     NOT NULL,
+    clientId   INTEGER     NOT NULL,
+    date       DATETIME    NOT NULL,
+    status     VARCHAR(15) NOT NULL,
 
     CONSTRAINT Purchase_PK PRIMARY KEY (purchaseId),
-    CONSTRAINT Purchase_service_FK FOREIGN KEY (serviceId) REFERENCES Service (serviceId),
-    CONSTRAINT Purchase_client_FK FOREIGN KEY (clientId) REFERENCES User (userId),
+    CONSTRAINT Purchase_service_FK FOREIGN KEY (serviceId) REFERENCES Service (serviceId) ON UPDATE CASCADE,
+    CONSTRAINT Purchase_client_FK FOREIGN KEY (clientId) REFERENCES User (userId) ON UPDATE CASCADE,
     CONSTRAINT Purchase_status_CK CHECK ( status IN ('pending', 'closed') )
 );
 
 CREATE TABLE Message
 (
     messageId  INTEGER,
-    senderId   INTEGER,
-    receiverId INTEGER,
-    serviceId  INTEGER,
-    content    TEXT NOT NULL,
-    date       DATE NOT NULL,
+    senderId   INTEGER  NOT NULL,
+    receiverId INTEGER  NOT NULL,
+    serviceId  INTEGER  NOT NULL,
+    content    TEXT     NOT NULL,
+    date       DATETIME NOT NULL,
 
     CONSTRAINT Message_PK PRIMARY KEY (messageId),
-    CONSTRAINT Message_sender_FK FOREIGN KEY (senderId) REFERENCES User (userId),
-    CONSTRAINT Message_receiver_FK FOREIGN KEY (receiverId) REFERENCES User (userId),
-    CONSTRAINT Message_service_FK FOREIGN KEY (serviceId) REFERENCES Service (serviceId)
+    CONSTRAINT Message_sender_FK FOREIGN KEY (senderId) REFERENCES User (userId) ON UPDATE CASCADE,
+    CONSTRAINT Message_receiver_FK FOREIGN KEY (receiverId) REFERENCES User (userId) ON UPDATE CASCADE,
+    CONSTRAINT Message_service_FK FOREIGN KEY (serviceId) REFERENCES Service (serviceId) ON UPDATE CASCADE
 );
 
 CREATE TABLE Feedback
@@ -95,10 +97,10 @@ CREATE TABLE Feedback
     purchaseId INTEGER       NOT NULL,
     rating     DECIMAL(2, 1) NOT NULL DEFAULT 0,
     review     TEXT,
-    date       DATE          NOT NULL,
+    date       DATETIME      NOT NULL,
 
     CONSTRAINT Feedback_PK PRIMARY KEY (feedbackId),
-    CONSTRAINT Feedback_purchase_FK FOREIGN KEY (purchaseId) REFERENCES Purchase (purchaseId),
+    CONSTRAINT Feedback_purchase_FK FOREIGN KEY (purchaseId) REFERENCES Purchase (purchaseId) ON UPDATE CASCADE,
     CONSTRAINT Feedback_rating_CK CHECK ( rating BETWEEN 0 AND 5 )
 );
 
@@ -107,8 +109,9 @@ CREATE TRIGGER update_service_rating_after_rating_insert
     ON Feedback
 BEGIN
     UPDATE Service
-    SET rating = COALESCE((SELECT AVG(rating)
-                           FROM Purchase
-                                    JOIN Feedback USING (purchaseId)), 0)
-    WHERE serviceId = NEW.feedbackId;
+    SET rating = COALESCE((SELECT AVG(f.rating)
+                           FROM Feedback f
+                                    JOIN Purchase p ON f.purchaseId = p.purchaseId
+                           WHERE p.serviceId = NEW.purchaseId), 0)
+    WHERE serviceId = (SELECT serviceId FROM Purchase WHERE purchaseId = NEW.purchaseId);
 END;
