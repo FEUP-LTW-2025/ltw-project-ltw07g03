71e71c54bbb2aa6fe554671bd32155c463f98f52
MarianaMorais29 <up202006319@up.pt>
2025-04-06 15:03:18 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<?php 
+declare(strict_types = 1); ?>
+
+<footer class="site-footer">
+    <div class="container">
+        <p>&copy; 2025 FLEXA. All rights reserved.</p>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<?php 
+declare(strict_types = 1); ?>
+
+<header class="site-header dark-header">
+    <div class="container header-flex">
+        <div class="logo">
+            <img
+            src="images/flexa-logo.png"
+            srcset="images/flexa-logo@2x.png 2x"
+            alt="FLEXA logo"
+            class="logo-img-big"
+        />
+        </div>
+        <nav>
+            <ul class="nav-menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+
+
