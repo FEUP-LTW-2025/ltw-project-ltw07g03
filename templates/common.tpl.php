7a19a4d181a87a0b97bbcbf9d7215b13527463a2
Pedro Castro <falecompedroac@gmail.com>
2025-04-07 18:31:59 +0100

    common templates

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -1,85 +1,48 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 require_once(__DIR__ . '/../utils/session.php');
 ?>
 
-    
-<?php function drawHeader(Session $session){ ?>
-
+<?php function drawHeader(string $title, Session $session) { ?>
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>DeepWeb Fiver</title>
-    </head>
-    <body>
-        <header>
-        <h1><a href="/">DeepWeb Fiver</a></h1>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "FLEXA :: " . $title ?></title>
+    <link href="/templates/style.css" rel="stylesheet">
+</head>
+<body>
+<header class="site-header dark-header">
+    <div class="container header-flex">
+        <div class="logo">
+            <img
+                    src="/assets/images/flexa-logo@2x.png"
+                    srcset="/assets/images/flexa-logo@2x.png 2x"
+                    alt="FLEXA logo"
+                    class="logo-img-big"
+            />
+        </div>
+        <nav>
+            <ul class="nav-menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<main>
+    <?php } ?>
 
-      <?php 
-        if ($session->isLoggedIn()) drawLogoutForm($session);
-        else drawLoginForm($session);
-
-      ?>
-        </header>
-
-
-        <section id="messages">
-            <?php foreach ($session->getMessages() as $messsage) { ?>
-                <article class="<?=$messsage['type']?>">
-                    <?=$messsage['text']?>
-                </article>
-            <?php } ?>
-    </section>
-
-
-    <main>
-
-<?php } ?>
-
-
-
-
-<?php function drawFooter() { ?>
-    </main>
-
-    <footer>
-      DeepWeb Fivver &copy; 2025
-    </footer>
-
-  </body>
+    <?php function drawFooter() { ?>
+</main>
+<footer class="site-footer">
+    <div class="container">
+        <p>&copy; 2025 FLEXA. All rights reserved.</p>
+    </div>
+</footer>
+</body>
 </html>
 <?php } ?>
-
-
-
-
-<?php function drawLoginForm(){ ?>
-    <form action="action_login.php" method="post" class="login">
-
-    <label>Email:</label>
-    <input type="email" name="email" placeholder="jhon123@gmail.com">
-
-    <label>Password:</label>
-    <input type="password" name="password" placeholder="password">
-
-    <a href="../pages/register.php">Register</a>
-    <button type="submit">Login</button>
-    
-</form>
-
-<?php } ?>
-
-
-
-<?php   function drawLogoutForm(Session $session){ ?>
-    <form action="../actions/action_logout.php" method="post" class="logout">
-            <a href="../pages/profile.php"><?php $session->getName()?></a>
-            <button type="submit">Logout</button>
-          </form>
-<?php } ?>
-
-
-
-
