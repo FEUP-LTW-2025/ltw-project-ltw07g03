726d058c34e611062b3247d91880a0fe48da13f6
ArturM <arturmoreira899@gmail.com>
2025-04-24 18:29:33 +0100

    frontend finished

---
+++ templates/checkout.tpl.php
@@ -0,0 +1,43 @@
+<?php function drawCheckoutForm($service_freelancer): void { ?>
+  <section class="checkout-section">
+
+  <div class="checkout-summary">
+    <h2>Confirm purchase</h2>
+
+    <img src="<?= htmlspecialchars($service_freelancer['images'][0] ?? '/assets/images/pfps/default.jpeg') ?>" 
+         alt="Service image" 
+         class="checkout-summary-img">
+
+    <p><strong>Service:</strong> 
+        <a href="/pages/service_detail.php?id=<?= $service_freelancer['serviceId'] ?>" class="checkout-link">
+            <?= htmlspecialchars($service_freelancer['title']) ?>
+        </a>
+    </p>
+
+    <p><strong>Freelancer:</strong> 
+        <a href="/pages/user.php?id=<?= $service_freelancer['freelancer']['id'] ?>" class="checkout-link">
+            <?= htmlspecialchars($service_freelancer['freelancer']['name']) ?>
+        </a>
+    </p>
+
+    <p><strong>Price:</strong> <?= htmlspecialchars((string)$service_freelancer['price']) ?> €</p>
+</div>
+
+    <form class="payment-info" action="/actions/action_purchase.php" method="post">
+
+      <label for="Card_number">Card Number</label>
+      <input type="text" id="card_number" name="card_number" placeholder="1234 5678 9012 3456" required autocomplete="cc-number">
+
+      <label for="exp_date">Expiration Date</label>
+      <input type="text" id="exp_date" name="exp_date" placeholder="MM/AA" required autocomplete="cc-exp">
+
+      <label for="sec_code">Security Code(CVV)</label>
+      <input type="text" id="sec_code" name="sec_code" placeholder="123" required autocomplete="cc-csc">
+
+      <label for="cardholder_name">Cardholder´s name</label>
+      <input type="text" id="cardholder_name" name="cardholder_name" placeholder="John Silva" required autocomplete="cc-name">
+
+      <input type="submit" value="Buy">
+    </form>
+  </section>
+<?php } ?>
