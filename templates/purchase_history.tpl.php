d9fbefc5803978ec44bb2cbd28146d995a24e6b5
Pedro Castro <falecompedroac@gmail.com>
2025-04-25 20:59:30 +0100

    styling and display date at history

--- templates/purchase_history.tpl.php
+++ templates/purchase_history.tpl.php
@@ -3,14 +3,16 @@ declare(strict_types=1);
 
 function drawPurchaseHistory(array $purchasesWithDetails): void
 { ?>
+    <h3>Your Purchases</h3>
     <div>
-        <h3>Your Purchases</h3>
         <?php if (!empty($purchasesWithDetails)): ?>
             <?php foreach ($purchasesWithDetails as $item): ?>
                 <div class="purchase-card">
+                    <img src="<?= $item["service"]["images"][0] ?>">
                     <p><strong>Service:</strong> <?= $item['service']['title'] ?></p>
                     <p><strong>Freelancer:</strong> <?= $item['service']['freelancer']['name'] ?></p>
                     <p><strong>Status:</strong> <?= $item['purchase']->getStatus() ?></p>
+                    <p><strong>Date:</strong> <?= gmdate("Y-m-d | H:i:s", $item['purchase']->getDate()) ?></p>
                 </div>
             <?php endforeach; ?>
         <?php else: ?>
