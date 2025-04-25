6a79b90d4aa45cafba4525fbdd1aee7a01f6f1cc
Pedro Castro <falecompedroac@gmail.com>
2025-04-25 12:35:22 +0100

    improved styling

--- templates/admin_dashboard.tpl.php
+++ templates/admin_dashboard.tpl.php
@@ -4,19 +4,20 @@ require_once(__DIR__ . '/../model/category.class.php');
 
 function drawAdminDashboard(array $categories): void
 { ?>
-    <section>
+    <section class="category-dashboard">
+        <h3>Create a new category</h3>
         <?php foreach ($categories as $cat): ?>
-            <?php $encoded = urlencode($cat->getName()); ?>
-            <a href="/pages/category.php?name=<?= $encoded ?>">
-                <?= $cat->getIcon() ?> <?= $cat->getName() ?>
+            <?php $encoded = $cat->getName(); ?>
+            <a class="category-box" href="/pages/category.php?name=<?= $encoded ?>">
+                <span class="category-icon"><?= $cat->getIcon() ?></span>
+                <span class="category-name"><?= $cat->getName() ?></span>
             </a>
         <?php endforeach; ?>
-        <h2>Create a new category</h2>
-        <form action="/actions/action_create_category.php" method="post">
+        <form class="add-category-form" action="/actions/action_create_category.php" method="post">
             <label for="name">Name</label>
-            <input type="text" name="name" placeholder="New category name">
-            <label for="icon">Icon</label>
-            <input type="text" name="icon" placeholder="New category icon">
+            <input type="text" name="name" placeholder="New category name" required>
+            <label for="icon">Icon (emoji)</label>
+            <input type="text" name="icon" placeholder="Paste emoji here" required>
             <input type="submit" value="Create">
         </form>
     </section>
