ea370b598ef9b2085005d1cc6db79dc977cda43d
ArturM <arturmoreira899@gmail.com>
2025-05-05 22:04:20 +0100

    template for the messages

--- pages/chat.php
+++ pages/chat.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 require_once(__DIR__ . '/../utils/session.php');
 require_once(__DIR__ . '/../templates/common.tpl.php');
-//require_once(__DIR__ . '/../templates/chat.tpl.php');
+require_once(__DIR__ . '/../templates/chat.tpl.php');
 require_once(__DIR__ . '/../database/connection.db.php');
 require_once(__DIR__ . '/../model/user.class.php');
 require_once(__DIR__ . '/../model/message.class.php');
@@ -32,5 +32,5 @@ if (!$client || !$freelancer) {
 $history = Message::getMessagesByParticipantsId($db, $client_id, $freelancer_id);
 
 drawHeader("Chat", $db, $session);
-//drawChat($client, $freelancer, $history);
+drawChat($client, $freelancer, $history);
 drawFooter();
