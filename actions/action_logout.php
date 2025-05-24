86873b624feae80db3414af5b4d9b83479f30635
Pedro Castro <falecompedroac@gmail.com>
2025-05-24 11:35:20 +0100

    securing login

--- actions/action_logout.php
+++ actions/action_logout.php
@@ -1,10 +1,9 @@
 <?php
 declare(strict_types=1);
 require_once(__DIR__ . '/../utils/session.php');
-require_once(__DIR__ . '/../database/connection.db.php');
-require_once(__DIR__ . '/../model/user.class.php');
 
 $session = new Session();
 
 $session->Logout();
-header('Location: ' . '/pages/index.php');
+header('Location: /pages/index.php');
+exit();
